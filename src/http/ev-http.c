583b3f336f4ebfc20ee3a3c97ade0108ee7b4b96
qgymib <qgymib@gmail.com>
2023-02-27 10:58:52 +0800

    add http library

--- src/http/ev-http.c
+++ src/http/ev-http.c
@@ -13,44 +13,80 @@
 #define ALIGN_WITH(size, align) \
     (((uintptr_t)(size) + ((uintptr_t)(align) - 1)) & ~((uintptr_t)(align) - 1))
 
+/**
+ * @brief Static initializer for #ev_http_str_t.
+ */
+#define EV_HTTP_STR_INIT    { NULL, 0, 0 }
+
 #if defined(_WIN32)
-#   define sscanf(b, f, ...)    sscanf_s(b, f, ##__VA_ARGS__)
+#   define sscanf(b, f, ...)        sscanf_s(b, f, ##__VA_ARGS__)
+#   define strncasecmp(s1, s2, n)   _strnicmp(s1, s2, n)
 #endif
+
+typedef enum ev_http_conn_action_type
+{
+    EV_HTTP_CONN_ACTION_SEND,
+    EV_HTTP_CONN_ACTION_SERVE,
+} ev_http_conn_action_type_t;
 
 typedef struct ev_http_send_token
 {
-    ev_tcp_write_req_t  token;
-    ev_http_conn_t*     conn;
-    size_t              size;
-
-#if defined(_MSC_VER)
-#   pragma warning(push)
-#   pragma warning(disable : 4200)
-#endif
-    unsigned char       data[];
-#if defined(_MSC_VER)
-#   pragma warning(pop)
-#endif
+    ev_tcp_write_req_t          token;  /**< Send token. */
+    ev_http_str_t               data;   /**< Data to send. No need to free. */
 } ev_http_send_token_t;
+
+typedef struct ev_http_serve_token
+{
+    ev_http_str_t               method;         /**< METHOD. No need to free. */
+    ev_http_str_t               url;            /**< URL. No need to free. */
+    ev_http_str_t               root_path;      /**< Root path. No need to free. */
+    ev_http_str_t               ssi_pattern;    /**< SSI. No need to free. */
+    ev_http_str_t               extra_headers;  /**< Extra headers. No need to free. */
+    ev_http_str_t               mime_types;     /**< MIME. No need to free. */
+    ev_http_str_t               page404;        /**< Path to 404 page. No need to free. */
+    ev_http_str_t               if_none_match;  /**< Value of `If-None-Match`. No need to free. */
+    ev_http_str_t               range;          /**< Value of `Range`. No need to free. */
+    ev_http_fs_t*               fs;             /**< File system instance. */
+
+    void*                       fd;             /**< File descriptor. */
+    size_t                      remain_size;    /**< How many bytes remain to send. */
+} ev_http_serve_token_t;
+
+typedef struct ev_http_conn_action
+{
+    ev_list_node_t              node;   /**< #ev_http_conn_t::send_queue */
+    ev_http_conn_t*             conn;   /**< The connection we are working on. */
+    ev_http_conn_action_type_t  type;   /**< Action type. */
+
+    union
+    {
+        ev_http_send_token_t    send;
+        ev_http_serve_token_t   serve;
+    }as;
+} ev_http_conn_action_t;
 
 struct ev_http_conn_s
 {
-    ev_list_node_t      node;                       /**< Node for #ev_http_t::client_table */
+    ev_list_node_t              node;                       /**< Node for #ev_http_t::client_table */
 
-    ev_http_t*          belong;                     /**< HTTP instance. */
-    ev_tcp_t            client_sock;                /**< Client socket. */
+    ev_http_t*                  belong;                     /**< HTTP instance. */
+    ev_tcp_t                    client_sock;                /**< Client socket. */
 
-    ev_tcp_read_req_t   recv_req;                   /**< Recv request token. */
-    char                recv_buf[EV_HTTP_IO_SIZE];  /**< Recv buffer */
+    ev_tcp_read_req_t           recv_req;                   /**< Recv request token. */
+    char                        recv_buf[EV_HTTP_IO_SIZE];  /**< Recv buffer */
 
-    ev_http_message_t*  on_parsing;                 /**< The message we are processing. */
-    llhttp_t            parser;                     /**< HTTP parser */
-    llhttp_settings_t   parser_setting;             /**< HTTP parser settings */
+    ev_http_message_t*          on_parsing;                 /**< The message we are processing. */
+    llhttp_t                    parser;                     /**< HTTP parser */
+    llhttp_settings_t           parser_setting;             /**< HTTP parser settings */
 
-    int                 need_cb;
-    ev_http_cb          evt_cb;
-    void*               evt_arg;
+    ev_list_t                   send_queue;                 /**< Send queue. */
+
+    int                         need_cb;
+    ev_http_cb                  evt_cb;
+    void*                       evt_arg;
 };
+
+static ev_http_str_t s_empty_str = EV_HTTP_STR_INIT;
 
 /**
  * @brief Ensure \p str have enough capacity for \p size.
@@ -94,6 +130,40 @@ static int s_uv_http_str_ensure_size(ev_http_str_t* str, size_t size)
     return 0;
 }
 
+static int s_uv_http_str_vprintf(ev_http_str_t* str, const char* fmt, va_list ap)
+{
+    va_list ap_bak;
+    va_copy(ap_bak, ap);
+    int ret = vsnprintf(NULL, 0, fmt, ap_bak);
+    va_end(ap_bak);
+
+    size_t required_cap = str->len + ret;
+    if (s_uv_http_str_ensure_size(str, required_cap) != 0)
+    {
+        return EV_ENOMEM;
+    }
+
+    if (vsnprintf(str->ptr + str->len, ret + 1, fmt, ap) != ret)
+    {
+        abort();
+    }
+    str->len += ret;
+
+    return ret;
+}
+
+static int s_uv_http_str_printf(ev_http_str_t* str, const char* fmt, ...)
+{
+    int ret;
+    va_list ap;
+    va_start(ap, fmt);
+    {
+        ret = s_uv_http_str_vprintf(str, fmt, ap);
+    }
+    va_end(ap);
+
+    return ret;
+}
 
 static void s_uv_http_str_destroy(ev_http_str_t* str)
 {
@@ -120,6 +190,136 @@ static int s_uv_http_str_append(ev_http_str_t* str, const void* at, size_t lengt
     str->len = required_size;
 
     return 0;
+}
+
+static void s_uv_http_fs_release(struct uv_http_fs* self)
+{
+    (void)self;
+}
+
+static int s_uv_http_fs_stat(struct uv_http_fs* self, const char* path, size_t* size, time_t* mtime)
+{
+    (void)self;
+#if defined(_WIN32)
+    struct _stat st;
+    if (_stat(path, &st) != 0)
+    {
+        return 0;
+    }
+    int is_dir = st.st_mode & _S_IFDIR;
+#else
+    struct stat st;
+    if (stat(path, &st) != 0)
+    {
+        return 0;
+    }
+    int is_dir = S_ISDIR(st.st_mode);
+#endif
+
+    if (size != NULL)
+    {
+        *size = st.st_size;
+    }
+    if (mtime != NULL)
+    {
+        *mtime = st.st_mtime;
+    }
+    return EV_HTTP_FS_READ | EV_HTTP_FS_WRITE | (is_dir ? EV_HTTP_FS_DIR : 0);
+}
+
+static void s_uv_http_fs_ls(struct uv_http_fs* self, const char* path,
+    void (*cb)(const char* path, void* arg), void* arg)
+{
+    (void)self;
+#if defined(_WIN32)
+
+    ev_http_str_t fix_path = EV_HTTP_STR_INIT;
+    s_uv_http_str_printf(&fix_path, "%s/*", path);
+
+    WIN32_FIND_DATAA find_data;
+    HANDLE dp = FindFirstFileA(fix_path.ptr, &find_data);
+    s_uv_http_str_destroy(&fix_path);
+    if (dp == INVALID_HANDLE_VALUE)
+    {
+        return;
+    }
+
+    do
+    {
+        if (strcmp(find_data.cFileName, ".") == 0 || strcmp(find_data.cFileName, "..") == 0)
+        {
+            continue;
+        }
+        cb(find_data.cFileName, arg);
+    } while (FindNextFileA(dp, &find_data));
+
+    FindClose(dp);
+
+#else
+    DIR* dir;
+    struct dirent* dp;
+
+    if ((dir = opendir(path)) == NULL)
+    {
+        return;
+    }
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+        {
+            continue;
+        }
+        cb(dp->d_name, arg);
+    }
+    closedir(dir);
+#endif
+}
+
+static void* s_uv_http_fs_open(struct uv_http_fs* self, const char* path, int flags)
+{
+    (void)self;
+    const char* mode = flags == EV_HTTP_FS_READ ? "rb" : "a+b";
+
+#if defined(_WIN32)
+    FILE* f;
+    if (fopen_s(&f, path, mode) != 0)
+    {
+        return NULL;
+    }
+    return (void*)f;
+#else
+    return (void*)fopen(path, mode);
+#endif
+}
+
+static void s_uv_http_fs_close(struct uv_http_fs* self, void* fd)
+{
+    (void)self;
+    fclose((FILE*)fd);
+}
+
+static int s_uv_http_fs_read(struct uv_http_fs* self, void* fd, void* buf, size_t size)
+{
+    (void)self;
+    return (int)fread(buf, 1, size, (FILE*)fd);
+}
+
+static int s_uv_http_fs_write(struct uv_http_fs* self, void* fd, const void* buf, size_t size)
+{
+    (void)self;
+    return (int)fwrite(buf, 1, size, (FILE*)fd);
+}
+
+static int s_uv_http_fs_seek(struct uv_http_fs* self, void* fd, size_t offset)
+{
+    (void)self;
+    int errcode = 0;
+    if (fseek(fd, (long)offset, SEEK_SET) != 0)
+    {
+        errcode = 0 - errno;
+    }
+    return errcode;
 }
 
 static void _ev_http_on_close(ev_tcp_t* sock)
@@ -373,7 +573,7 @@ static int _ev_http_on_parser_begin(llhttp_t* parser)
     }
     memset(conn->on_parsing, 0, sizeof(*conn->on_parsing));
 
-    size_t malloc_size = sizeof(uv_http_header_t) * (default_header_cap);
+    size_t malloc_size = sizeof(ev_http_header_t) * (default_header_cap);
     if ((conn->on_parsing->headers = malloc(malloc_size)) == NULL)
     {
         return EV_ENOMEM;
@@ -510,6 +710,21 @@ static int _ev_http_try_accept(ev_http_t* http, ev_http_conn_t** conn)
     return 0;
 }
 
+static void _ev_http_on_send(ev_tcp_write_req_t* req, size_t size, int stat)
+{
+    (void)size;
+    ev_http_send_token_t* token = EV_CONTAINER_OF(req, ev_http_send_token_t, token);
+    ev_http_conn_action_t* action = EV_CONTAINER_OF(token, ev_http_conn_action_t, as.send);
+    ev_http_conn_t* conn = action->conn;
+
+    free(token);
+
+    if (stat != 0)
+    {
+        _ev_http_close_connection(conn, 1);
+    }
+}
+
 int ev_http_init(ev_loop_t* loop, ev_http_t* http)
 {
     int ret;
@@ -562,40 +777,260 @@ int ev_http_close(ev_http_conn_t* conn)
     return 0;
 }
 
-static void _ev_http_on_send(ev_tcp_write_req_t* req, size_t size, int stat)
+ev_http_str_t* ev_http_get_header(ev_http_message_t* msg, const char* name)
 {
-    (void)size;
-    ev_http_send_token_t* token = EV_CONTAINER_OF(req, ev_http_send_token_t, token);
-    ev_http_conn_t* conn = token->conn;
+    size_t i;
+    size_t name_len = strlen(name);
 
-    free(token);
-
-    if (stat != 0)
+    for (i = 0; i < msg->header_len; i++)
     {
-        _ev_http_close_connection(conn, 1);
+        ev_http_header_t* hdr = &msg->headers[i];
+        if (hdr->name.len == name_len && strncasecmp(hdr->name.ptr, name, name_len) == 0)
+        {
+            return &hdr->value;
+        }
     }
+
+    return NULL;
+}
+
+static int s_uv_http_parse_range(const ev_http_str_t* str, size_t size,
+    size_t* beg, size_t* end)
+{
+    unsigned long long a, b;
+    if (str->len < 6 || memcmp(str->ptr, "bytes=", 6) != 0)
+    {
+        return EV_ENOENT;
+    }
+
+    const char* p_beg = str->ptr + 6;
+    const char* p_end = strstr(p_beg, ",");
+    p_end = p_end != NULL ? p_end : str->ptr + str->len;
+
+    const char* p_minus = strstr(p_beg, "-");
+
+    /* last n bytes */
+    if (p_minus == p_beg)
+    {
+        if (sscanf(p_beg, "-%llu", &a) != 1)
+        {
+            return EV_EINVAL;
+        }
+
+        if (a > size)
+        {
+            return EV_EINVAL;
+        }
+
+        *beg = size - a;
+        *end = size;
+        return 0;
+    }
+
+    /* start from n */
+    if (p_minus == p_end)
+    {
+        if (sscanf(p_beg, "%llu-", &a) != 1)
+        {
+            return EV_EINVAL;
+        }
+        if (a > size)
+        {
+            return EV_EINVAL;
+        }
+
+        *beg = a;
+        *end = size;
+        return 0;
+    }
+
+    if (sscanf(p_beg, "%llu-%llu", &a, &b) != 2)
+    {
+        return EV_EINVAL;
+    }
+    if (a > b || b >= size)
+    {
+        return EV_EINVAL;
+    }
+
+    *beg = a;
+    *end = b;
+    return 0;
+}
+
+static int _ev_http_action_conn_send(ev_http_conn_t* conn, ev_http_conn_action_t* action)
+{
+    ev_buf_t buf = ev_buf_make(action->as.send.data.ptr, action->as.send.data.len);
+    int ret = ev_tcp_write(&conn->client_sock, &action->as.send.token, &buf, 1, _ev_http_on_send);
+    ev_list_erase(&conn->send_queue, &action->node);
+
+    return ret;
+}
+
+static int _ev_http_action_conn_serve(ev_http_conn_t* conn, ev_http_conn_action_t* action)
+{
+    ev_http_serve_token_t* serve = &action->as.serve;
+
+    
+}
+
+static int _ev_http_action_connection(ev_http_conn_t* conn)
+{
+    ev_http_conn_action_t* action = ev_list_begin(&conn->send_queue);
+    if (action == NULL)
+    {
+        return 0;
+    }
+
+    if (action->type == EV_HTTP_CONN_ACTION_SEND)
+    {
+        return _ev_http_action_conn_send(conn, action);
+    }
+
+    return _ev_http_action_conn_serve(conn, action);
 }
 
 int ev_http_send(ev_http_conn_t* conn, const void* data, size_t size)
 {
-    size_t malloc_size = sizeof(ev_http_send_token_t) + size;
-    ev_http_send_token_t* token = malloc(malloc_size);
-    if (token == NULL)
+    size_t malloc_size = sizeof(ev_http_conn_action_t) + size;
+    ev_http_conn_action_t* action = malloc(malloc_size);
+    if (action == NULL)
     {
         return EV_ENOMEM;
     }
 
-    memcpy(token->data, data, size);
-    token->size = size;
-    token->conn = conn;
+    action->conn = conn;
+    action->type = EV_HTTP_CONN_ACTION_SEND;
+    action->as.send.data.ptr = (char*)(action + 1);
+    action->as.send.data.len = size;
+    action->as.send.data.cap = 0;
+    memcpy(action->as.send.data.ptr, data, size);
 
-    ev_buf_t buf = ev_buf_make(token->data, token->size);
-    int ret = ev_tcp_write(&conn->client_sock, &token->token, &buf, 1, _ev_http_on_send);
-    if (ret != 0)
+    ev_list_push_back(&conn->send_queue, &action->node);
+
+    return _ev_http_action_connection(conn);
+}
+
+/**
+ * @brief Generate serve file response message.
+ * @param[in] conn  HTTP connection.
+ * @param[in] msg   HTTP incoming message.
+ * @param[in] cfg   Serve dir options.
+ * @return          UV error code.
+ */
+int ev_http_serve_file(ev_http_conn_t* conn, ev_http_message_t* msg,
+    ev_http_serve_cfg_t* cfg)
+{
+    static ev_http_fs_t s_builtin_fs = {
+        s_uv_http_fs_release,
+        s_uv_http_fs_stat,
+        s_uv_http_fs_ls,
+        s_uv_http_fs_open,
+        s_uv_http_fs_close,
+        s_uv_http_fs_read,
+        s_uv_http_fs_write,
+        s_uv_http_fs_seek,
+    };
+
+    ev_http_str_t* if_none_match = ev_http_get_header(msg, "If-None-Match");
+    if_none_match = if_none_match != NULL ? if_none_match : &s_empty_str;
+    ev_http_str_t* range = ev_http_get_header(msg, "Range");
+    range = range != NULL ? range : &s_empty_str;
+
+    size_t root_path_len = strlen(cfg->root_path);
+    size_t ssi_pattern_len = cfg->ssi_pattern != NULL ? strlen(cfg->ssi_pattern) : 0;
+    size_t extra_headers_len = cfg->extra_headers != NULL ? strlen(cfg->extra_headers) : 0;
+    size_t mime_types_len = cfg->mime_types != NULL ? strlen(cfg->mime_types) : 0;
+    size_t page404_len = cfg->page404 != NULL ? strlen(cfg->page404) : 0;
+    size_t if_none_match_len = if_none_match->len;
+    size_t range_len = range->len;
+
+    size_t malloc_size = sizeof(ev_http_conn_action_t)
+        + msg->method.len + 1
+        + msg->url.len + 1
+        + root_path_len + 1
+        + ssi_pattern_len + 1
+        + extra_headers_len + 1
+        + mime_types_len + 1
+        + page404_len + 1
+        + if_none_match_len + 1
+        + range_len + 1;
+    ev_http_conn_action_t* action = malloc(malloc_size);
+    if (action == NULL)
     {
-        free(token);
-        return ret;
+        return EV_ENOMEM;
     }
+    action->conn = conn;
+    action->type = EV_HTTP_CONN_ACTION_SERVE;
 
-    return 0;
+    memset(&action->as.serve, 0, sizeof(action->as.serve));
+    char* pos = (char*)(action + 1);
+
+    action->as.serve.method.cap = 0;
+    action->as.serve.method.len = msg->method.len;
+    action->as.serve.method.ptr = pos;
+    memcpy(action->as.serve.method.ptr, msg->method.ptr, msg->method.len);
+    action->as.serve.method.ptr[action->as.serve.method.len] = '\0';
+    pos += msg->method.len + 1;
+
+    action->as.serve.url.cap = 0;
+    action->as.serve.url.len = msg->url.len;
+    action->as.serve.url.ptr = pos;
+    memcpy(action->as.serve.url.ptr, msg->url.ptr, msg->url.len);
+    action->as.serve.url.ptr[action->as.serve.url.len] = '\0';
+    pos += msg->url.len + 1;
+
+    action->as.serve.root_path.cap = 0;
+    action->as.serve.root_path.len = root_path_len;
+    action->as.serve.root_path.ptr = pos;
+    memcpy(action->as.serve.root_path.ptr, cfg->root_path, root_path_len);
+    action->as.serve.root_path.ptr[root_path_len] = '\0';
+    pos += root_path_len + 1;
+
+    action->as.serve.ssi_pattern.cap = 0;
+    action->as.serve.ssi_pattern.len = ssi_pattern_len;
+    action->as.serve.ssi_pattern.ptr = pos;
+    memcpy(action->as.serve.ssi_pattern.ptr, cfg->ssi_pattern, ssi_pattern_len);
+    action->as.serve.ssi_pattern.ptr[ssi_pattern_len] = '\0';
+    pos += ssi_pattern_len + 1;
+
+    action->as.serve.extra_headers.cap = 0;
+    action->as.serve.extra_headers.len = extra_headers_len;
+    action->as.serve.extra_headers.ptr = pos;
+    memcpy(action->as.serve.extra_headers.ptr, cfg->extra_headers, extra_headers_len);
+    action->as.serve.extra_headers.ptr[extra_headers_len] = '\0';
+    pos += extra_headers_len + 1;
+
+    action->as.serve.mime_types.cap = 0;
+    action->as.serve.mime_types.len = mime_types_len;
+    action->as.serve.mime_types.ptr = pos;
+    memcpy(action->as.serve.mime_types.ptr, cfg->mime_types, mime_types_len);
+    action->as.serve.mime_types.ptr[mime_types_len] = '\0';
+    pos += mime_types_len + 1;
+
+    action->as.serve.page404.cap = 0;
+    action->as.serve.page404.len = page404_len;
+    action->as.serve.page404.ptr = pos;
+    memcpy(action->as.serve.page404.ptr, cfg->page404, page404_len);
+    action->as.serve.page404.ptr[page404_len] = '\0';
+    pos += page404_len + 1;
+
+    action->as.serve.if_none_match.cap = 0;
+    action->as.serve.if_none_match.len = if_none_match_len;
+    action->as.serve.if_none_match.ptr = pos;
+    memcpy(action->as.serve.if_none_match.ptr, if_none_match->ptr, if_none_match_len);
+    action->as.serve.if_none_match.ptr[if_none_match_len] = '\0';
+    pos += if_none_match_len + 1;
+
+    action->as.serve.range.cap = 0;
+    action->as.serve.range.len = range_len;
+    action->as.serve.range.ptr = pos;
+    memcpy(action->as.serve.range.ptr, range->ptr, range_len);
+    action->as.serve.range.ptr[range_len] = '\0';
+    pos += range_len + 1;
+
+    action->as.serve.fs = cfg->fs != NULL ? cfg->fs : &s_builtin_fs;
+
+    ev_list_push_back(&conn->send_queue, &action->node);
+    return _ev_http_action_connection(conn);
 }
--- src/http/ev-http.h
+++ src/http/ev-http.h
@@ -19,6 +19,13 @@ typedef enum ev_http_event
     EV_HTTP_MESSAGE,    /**< (#ev_http_message_t) HTTP request/response */
 } ev_http_event_t;
 
+typedef enum uv_http_fs_flag
+{
+    EV_HTTP_FS_READ     = 1,
+    EV_HTTP_FS_WRITE    = 2,
+    EV_HTTP_FS_DIR      = 4,
+} uv_http_fs_flag_t;
+
 typedef struct ev_http_str
 {
     char*               ptr;            /**< String address. */
@@ -30,7 +37,7 @@ typedef struct
 {
     ev_http_str_t       name;           /**< Header name. */
     ev_http_str_t       value;          /**< Header value. */
-} uv_http_header_t;
+} ev_http_header_t;
 
 typedef struct ev_http_message_s
 {
@@ -39,7 +46,7 @@ typedef struct ev_http_message_s
     ev_http_str_t       status;         /**< HTTP status. */
     ev_http_str_t       version;        /**< HTTP version. */
 
-    uv_http_header_t*   headers;        /**< HTTP header array. */
+    ev_http_header_t*   headers;        /**< HTTP header array. */
     size_t              header_len;     /**< HTTP header array length. */
     size_t              header_cap;     /**< HTTP header array capacity. */
 
@@ -60,6 +67,89 @@ typedef void (*ev_http_cb)(ev_http_conn_t* conn, int evt, void* evt_data, void* 
  * @param[in] http  HTTP component.
  */
 typedef void (*ev_http_close_cb)(ev_http_t* http);
+
+typedef struct ev_http_fs
+{
+    /**
+     * @brief This instance is no longer needed.
+     * @param[in] self  Filesystem instance.
+     */
+    void (*release)(struct ev_http_fs* self);
+
+    /**
+     * @brief Stat path.
+     * @param[in] self      This object.
+     * @param[in] path      Path to file or directory.
+     * @param[out] size     Size of file or directory.
+     * @param[out] mtime    Modify time.
+     * @return              Bit-OR of #ev_http_fs_flag_t.
+     */
+    int (*stat)(struct ev_http_fs* self, const char* path, size_t* size, time_t* mtime);
+
+    /**
+     * @brief List file and directory in \p path.
+     * @param[in] self      This object.
+     * @param[in] path      Path to directory.
+     * @param[in] cb        Callback.
+     * @param[in] arg       User defined argument passed to \p cb.
+     */
+    void (*ls)(struct ev_http_fs* self, const char* path, void (*cb)(const char* path, void* arg), void* arg);
+
+    /**
+     * @brief Open file.
+     * @param[in] self      This object.
+     * @param[in] path      File path.
+     * @param[in] flags     Bit-OR of #ev_http_fs_flag_t.
+     * @return              File handle.
+     */
+    void* (*open)(struct ev_http_fs* self, const char* path, int flags);
+
+    /**
+     * @brief Close file.
+     * @param[in] self      This object.
+     * @param[in] fd        File handle.
+     */
+    void (*close)(struct ev_http_fs* self, void* fd);
+
+    /**
+     * @brief Read file.
+     * @param[in] self      This object.
+     * @param[in] fd        File handle.
+     * @param[in] buf       Buffer to store file content.
+     * @param[in] size      Buffer size.
+     * @return              The number of bytes read, or UV error code.
+     */
+    int (*read)(struct ev_http_fs* self, void* fd, void* buf, size_t size);
+
+    /**
+     * @brief Write file.
+     * @param[in] self      This object.
+     * @param[in] fd        File handle.
+     * @param[in] buf       Buffer to write.
+     * @param[in] size      Buffer size.
+     * @return              The number of bytes write, or UV error code.
+     */
+    int (*write)(struct ev_http_fs* self, void* fd, const void* buf, size_t size);
+
+    /**
+     * @brief Seek file.
+     * @param[in] self      This object.
+     * @param[in] fd        File handle.
+     * @param[in] offset    Offset from file begining.
+     * @return              UV error code.
+     */
+    int (*seek)(struct ev_http_fs* self, void* fd, size_t offset);
+} ev_http_fs_t;
+
+typedef struct ev_http_serve_cfg
+{
+    const char*     root_path;          /**< Web root directory, must be non-NULL. */
+    const char*     ssi_pattern;        /**< (Optional) SSI file name pattern. */
+    const char*     extra_headers;      /**< (Optional) Extra HTTP headers to add in responses. */
+    const char*     mime_types;         /**< (Optional) Extra mime types */
+    const char*     page404;            /**< (Optional) Path to the 404 page. */
+    ev_http_fs_t*   fs;                 /**< (Optional) Filesystem instance. */
+} ev_http_serve_cfg_t;
 
 struct ev_http_s
 {
@@ -107,6 +197,14 @@ int ev_http_listen(ev_http_t* http, const char* url, ev_http_cb cb, void* arg);
 int ev_http_close(ev_http_conn_t* conn);
 
 /**
+ * @brief Get header value.
+ * @param[in] msg   HTTP message.
+ * @param[in] name  Field name
+ * @return          Field value.
+ */
+ev_http_str_t* ev_http_get_header(ev_http_message_t* msg, const char* name);
+
+/**
  * @brief Send data on http connection.
  * @param[in] conn  HTTP connection.
  * @param[in] data  Data.
@@ -114,6 +212,16 @@ int ev_http_close(ev_http_conn_t* conn);
  * @return          UV error code.
  */
 int ev_http_send(ev_http_conn_t* conn, const void* data, size_t size);
+
+/**
+ * @brief Generate serve file response message.
+ * @param[in] conn  HTTP connection.
+ * @param[in] msg   HTTP incoming message.
+ * @param[in] cfg   Serve dir options.
+ * @return          UV error code.
+ */
+int ev_http_serve_file(ev_http_conn_t* conn, ev_http_message_t* msg,
+    ev_http_serve_cfg_t* cfg);
 
 #ifdef __cplusplus
 }
